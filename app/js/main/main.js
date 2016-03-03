aedfd76c48377742c3872add9e8ffa6629773ef3
Lutreer <lxuidesigner@gmail.com>
2016-03-03 20:24:54 +0800

    enter skill

--- app/js/main/main.js
+++ app/js/main/main.js
@@ -1,1 +1,14 @@
 'use strict';
+window.onload = function(){
+    setTimeout(function(){
+        $(".rotate-loading").hide();
+        $(".head-logo").addClass("head-logo-touch");
+    },1500);
+}
+
+$(".head-logo").tap(function(){
+    $(".layer-up").addClass("layer-up-remove");
+    $(".layer-down").addClass("layer-down-remove");
+    $(".head-logo").removeClass("head-logo-touch");
+    $(".head-logo").addClass("head-logo-move");
+})
